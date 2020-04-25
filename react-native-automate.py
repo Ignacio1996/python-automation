7c2e8e2e0306b1799964d40884d7b39d3ca48681
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-25 17:51:11 -0500

    almost working

--- react-native-automate.py
+++ react-native-automate.py
@@ -5,12 +5,12 @@ project_name = raw_input('Enter the project name: ')
 go_dir = 'cd ~ && cd projects && cd python && '
 init_react_native = 'npx react-native init ' + project_name
 go_into_app = ' && cd ' + project_name + ' && pwd'
-start_app = ' && npx react-native run-ios && npx react-native run-android && '
+start_app = ' && npx react-native run-ios && source ~/.bash_profile && npx react-native run-android && '
 open_project = 'code .'
 
 #Command to Build Project
 system_command = go_dir + init_react_native + go_into_app + start_app + open_project
-print(system_command)
+print("Full command" + system_command)
 
 #Build project
 os.system(system_command)
