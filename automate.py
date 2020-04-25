0353e68317b3a13e1abf013193b00917e9c56eeb
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-25 16:54:08 -0500

    adding project works

--- automate.py
+++ automate.py
@@ -10,8 +10,16 @@ go_into_dir = 'cd ' + project_name + ' && '
 index_file = 'touch index.js && npm init -y && npm i express body-parser && '
 open_project = 'code .'
 
+#Command to Build Project
 system_command = go_root + go_projects + go_python + create_dir + go_into_dir + index_file + open_project
-# system_command = 'cd ~ && cd projects && cd python && mkdir ' +  project_name + ' && cd ' + project_name + ' && touch index.js && code .'
 print(system_command)
+
+#Build project
 os.system(system_command)
+
+# Write Files
+user = 'nicolas'
+location = '/Users/' + user + '/projects/python/' + project_name + '/index.js'
+f = open(location, "w")
+f.write("console.log('New project!!'); \nconsole.log('The test worked!');")
 print('Your project has been initialized!')
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('New project!!'); 
+console.log('The test worked!');
