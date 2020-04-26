02cfdad48fcb297e66f68f2af36a0930dd3ab447
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-26 17:19:28 -0500

    more complex navigation working

---
+++ sarples/screens/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './Home';
+import Profile from './Profile';
+import Login from './Login';
+import SignUp from './SignUp';
+import Details from './Details';
+
+export {Home, Profile, Login, SignUp, Details}
