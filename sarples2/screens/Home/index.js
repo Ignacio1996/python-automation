08ff626e6444a44f2b8a475d98cd5a85c87a5a7f
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-26 18:28:27 -0500

    added fonts

--- sarples2/screens/Home/index.js
+++ sarples2/screens/Home/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Platform, StyleSheet} from 'react-native';
 function Screen(props) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      <Button title="Go to Home" onPress={()=>props.navigation.navigate("Details")}/>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={styles.text}>Home is like sunshine in the rain</Text>
+      <Text style={styles.normalText}>Home is like sunshine in the rain</Text>
+      <Text style={styles.textUbuntu}>
+        Home is like sunshine in the rainfall
+      </Text>
+      {/* <Text style={{fontFamily: 'Arial'}}>Home</Text> */}
+      <Button
+        title="Go to Home"
+        onPress={() => props.navigation.navigate('Details')}
+      />
     </View>
   );
 }
 export default Screen;
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: Platform.OS === 'ios' ? 'Roboto-Regular' : 'Roboto-Regular.ttf',
+    fontSize: 30,
+  },
+  textUbuntu: {
+    fontFamily: Platform.OS === 'ios' ? 'Ubuntu-Medium' : 'Ubuntu-Medium.ttf',
+    fontSize: 30,
+  },
+  normalText: {
+    // fontFamily: 'Georgia',
+    fontSize: 30,
+  },
+});
