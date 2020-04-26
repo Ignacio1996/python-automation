bd280feb66d966d2ce04a558832fb6be4addef0b
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-26 11:49:03 -0500

    added stuff

--- react-native-automate.py
+++ react-native-automate.py
@@ -3,15 +3,15 @@ import os
 project_name = raw_input('Enter the project name: ')
 
 go_dir = 'cd ~ && cd projects && cd python && '
-init_react_native = 'npx react-native init ' + project_name
-go_into_app = ' && cd ' + project_name + ' && pwd'
-start_app = ' && npx react-native run-ios && source ~/.bash_profile && npx react-native run-android && '
+init_react_native = 'npx react-native init ' + project_name + ' && '
+go_into_app = 'cd ' + project_name + ' && pwd && '
 react_navigation = 'npm install @react-navigation/native react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view @react-navigation/stack && '
-pod_install = 'cd ios && pod install && cd .. &&'
+pod_install = 'cd ios && pod install && cd .. && '
+start_app = 'npx react-native run-ios && source ~/.bash_profile && npx react-native run-android && '
 open_project = 'code .'
 
 #Command to Build Project
-system_command = go_dir + init_react_native + go_into_app + start_app + react_navigation + pod_install + open_project
+system_command = go_dir + init_react_native + go_into_app + react_navigation + pod_install + start_app + open_project
 print("Full command" + system_command)
 
 #Build project
