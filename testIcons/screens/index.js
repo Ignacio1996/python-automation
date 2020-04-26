61190fd938a298c77ab05c4322adde7ca48b3ab1
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-26 17:49:40 -0500

    added stack icon that opens drawer

--- testIcons/screens/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Home from './Home';
-import Profile from './Profile';
-import Login from './Login';
-import SignUp from './SignUp';
-import Details from './Details';
-
-export {Home, Profile, Login, SignUp, Details}
