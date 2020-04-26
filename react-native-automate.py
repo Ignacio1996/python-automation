e99544564889226dea3585c9966cb09da0cd32e5
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-26 12:08:24 -0500

    hope this works creating a new file

--- react-native-automate.py
+++ react-native-automate.py
@@ -6,12 +6,14 @@ go_dir = 'cd ~ && cd projects && cd python && '
 init_react_native = 'npx react-native init ' + project_name + ' && '
 go_into_app = 'cd ' + project_name + ' && pwd && '
 react_navigation = 'npm install @react-navigation/native react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view @react-navigation/stack && '
+firebase = 'npm i firebase && '
 pod_install = 'cd ios && pod install && cd .. && '
+auth_navigation = 'mkdir navigation && cd navigation && mkdir AuthNavigation && touch AuthNavigation/index.js && '
 start_app = 'npx react-native run-ios && source ~/.bash_profile && npx react-native run-android && '
 open_project = 'code .'
 
 #Command to Build Project
-system_command = go_dir + init_react_native + go_into_app + react_navigation + pod_install + start_app + open_project
+system_command = go_dir + init_react_native + go_into_app + react_navigation + firebase + pod_install + auth_navigation + start_app + open_project
 print("Full command" + system_command)
 
 #Build project
@@ -20,19 +22,20 @@ os.system(system_command)
 # Write Files
 user = 'nicolas'
 location = '/Users/' + user + '/projects/python/' + project_name + '/App.js'
-f = open(location, "w")
+app_file = open(location, "w")
 
 app_js = """
 import 'react-native-gesture-handler';
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen(props) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button title="Go to Details" onPress={()=>props.navigation.navigate("Details")}/>
     </View>
   );
 }
@@ -40,6 +43,7 @@ function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={()=>props.navigation.navigate("Home")}/>
     </View>
   );
 }
@@ -68,5 +72,9 @@ const styles = StyleSheet.create({
 });
 """
 
-f.write(app_js)
+authentication_stack = """
+
+"""
+
+app_file.write(app_js)
 print('Your project has been initialized with react navigation!')
